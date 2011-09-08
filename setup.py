b04ffc7e296089a5160b7796d05617937df57d26
Pedro Lucas Porcellis <pedrolucasporcellis@gmail.com>
2011-09-08 03:25:07 -0300

    commit pomodoro

---
+++ setup.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: latin1 -*-
+
+"""Build tar.gz for pomodoro-indicator.
+Needed packages to run (using Debian/Ubuntu package names):
+    python-appindicator  0.3.0-0ubuntu1
+    python-gobject       2.28.3-1ubuntu1.1
+    python-notify        0.1.1-2build4
+    python-gtk2-dev      0.1.1-2build4
+"""
+
+
+import os
+
+from distutils.command.install import install
+from distutils.core import setup
+
+
+class CustomInstall(install):
+    """Custom installation class on package files.
+
+    It copies all the files into the "PREFIX/share/PROJECTNAME" dir.
+    """
+    def run(self):
+        """Run parent install, and then save the install dir in the script."""
+        install.run(self)
+
+        for script in self.distribution.scripts:
+            script_path = os.path.join(self.install_scripts,
+                                       os.path.basename(script))
+            with open(script_path, 'rb') as fh:
+                content = fh.read()
+            content = content.replace('@ INSTALLED_BASE_DIR @',
+                                      self._custom_data_dir)
+            with open(script_path, 'wb') as fh:
+                fh.write(content)
+
+
+    def finalize_options(self):
+        """Alter the installation path."""
+        install.finalize_options(self)
+
+        # the data path is under 'prefix'
+        data_dir = os.path.join(self.prefix, "share",
+                                self.distribution.get_name())
+
+        # if we have 'root', put the building path also under it (used normally
+        # by pbuilder)
+        if self.root is None:
+            build_dir = data_dir
+        else:
+            build_dir = os.path.join(self.root, data_dir[1:])
+
+        # change the lib install directory so all package files go inside here
+        self.install_lib = build_dir
+
+        # save this custom data dir to later change the scripts
+        self._custom_data_dir = data_dir
+
+
+setup(
+    name = 'pomodoro-indicator',
+    version = '0.0.1',
+    license = 'GPL-3',
+    author = 'Pedro Lucas Porcellis',
+    author_email = 'pedrolucasporcellis@gmail.com',
+    description = 'Pomodoro technique app indicator.',
+    long_description = 'Pomodoro technique app indicator',
+    url = 'http://www.twitter.com/pedrolucasp',
+
+    packages = ["pomodoro"],
+    package_data = {"pomodoro": ["images/*.png"]},
+    scripts = ["bin/pomodoro-indicator"],
+
+    cmdclass = {
+        'install': CustomInstall,
+    }
+)
